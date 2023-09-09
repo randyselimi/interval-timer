64af9f4523fa4cab577a34cb0debd7c70f474a11
Randy Selimi <selimi.4@osu.edu>
2023-09-08 20:50:29 -0400

    chore: added sounds

--- src/lib/server/tile-data.ts
+++ src/lib/server/tile-data.ts
@@ -1,24 +1,158 @@
 export const tiles = [
   {
-    name: 'Ten Seconds',
-    sound: '3719__noisecollector__909-hat-open.wav',
+    name: 'One Second',
+    sound: 'tick1.wav',
     type: 'timer',
     x: 0,
     y: 0,
+    duration: 1000
+  },
+  {
+    name: 'Ten Seconds',
+    sound: 'snare2.wav',
+    type: 'timer',
+    x: 0,
+    y: 1,
     duration: 10000
   },
   {
-    name: 'Creep Wave Spawned',
-    sound: '3722__noisecollector__909-hat2.wav',
+    name: 'Creeps Spawned',
+    sound: 'tts',
     x: 0,
-    y: 1,
+    y: 2,
     duration: 30000
   },
   {
-    name: 'New Minute',
-    sound: '3723__noisecollector__909-kick-long.wav',
+    name: 'Pull',
+    sound: 'tts',
     x: 0,
-    y: 2,
-    duration: 60000
+    y: 3,
+    duration: 15000
+  },
+  {
+    name: 'Stack Pull',
+    sound: 'tts',
+    x: 1,
+    y: 3,
+    duration: 30000
+  },
+  {
+    name: 'One Minute',
+    sound: 'beep2.wav',
+    x: 2,
+    y: 3,
+    duration: 15000
+  },
+  {
+    name: 'River Rune in 20',
+    sound: 'tts',
+    x: 0,
+    y: 4,
+    duration: 100000
+  },
+  {
+    name: 'River Rune Spawned',
+    sound: 'power1.wav',
+    x: 1,
+    y: 4,
+    duration: 20000
+  },
+  {
+    name: 'Bounty Lotus in 20',
+    sound: 'tts',
+    x: 0,
+    y: 5,
+    duration: 160000
+  },
+  {
+    name: 'Bounty Lotus Spawned',
+    sound: 'coin1.wav',
+    x: 1,
+    y: 5,
+    duration: 20000
+  },
+  {
+    name: 'Night Catapult in 20',
+    sound: 'tts',
+    x: 0,
+    y: 6,
+    duration: 280000
+  },
+  {
+    name: 'Night Catapult Spawned',
+    sound: 'synth1.wav',
+    x: 1,
+    y: 6,
+    duration: 20000
+  },
+  {
+    name: 'Day Catapult in 20',
+    sound: 'tts',
+    x: 2,
+    y: 6,
+    duration: 280000
+  },
+  {
+    name: 'Day Catapult Spawned',
+    sound: 'synth1.wav',
+    x: 3,
+    y: 6,
+    duration: 20000
+  },
+  {
+    name: 'Wisdom Rune in 20',
+    sound: 'tts',
+    x: 0,
+    y: 7,
+    duration: 400000
+  },
+  {
+    name: 'Wisdom Rune Spawned',
+    sound: 'power2.wav',
+    x: 1,
+    y: 7,
+    duration: 20000
+  },
+  {
+    name: 'Tormentor Spawned',
+    sound: 'tts',
+    x: 0,
+    y: 8,
+    duration: 1200000
+  },
+  {
+    name: 'New Neutral Items',
+    sound: 'tts',
+    x: 0,
+    y: 9,
+    duration: 420000
+  },
+  {
+    name: 'New Neutral Items',
+    sound: 'tts',
+    x: 1,
+    y: 9,
+    duration: 600000
+  },
+  {
+    name: 'New Neutral Items',
+    sound: 'tts',
+    x: 2,
+    y: 9,
+    duration: 600000
+  },
+  {
+    name: 'New Neutral Items',
+    sound: 'tts',
+    x: 3,
+    y: 9,
+    duration: 600000
+  },
+  {
+    name: 'New Neutral Items',
+    sound: 'tts',
+    x: 4,
+    y: 9,
+    duration: 580000
   }
 ];
